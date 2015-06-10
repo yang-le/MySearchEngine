9edaeaeab72abc3a088aa4e556d9982f0c24866b
Yang Le <yangle0125@qq.com>
2015-06-10 21:07:54 +0800

    add score functions

--- searchengine.py
+++ searchengine.py
@@ -140,7 +140,12 @@ class searcher:
 	def getscoredlist(self, rows, wordids):
 		totalscores=dict([(row[0], 0) for row in rows])
 		
-		weights=[]
+		weights=[
+			(1.0, self.frequencyscore(rows))
+			,(1.5, self.locationscore(rows))
+			,(1.0, self.distancescore(rows))
+			,(1.0, self.inboundlinkscore(rows))
+		]
 
 		for (weight, scores) in weights:
 			for url in totalscores:
@@ -157,4 +162,42 @@ class searcher:
 		rankedscores=sorted([(score, url) for (url, score) in scores.items()], reverse=1)
 		for (score, urlid) in rankedscores[0:10]:
 			print '%f\t%s' % (score, self.geturlname(urlid))
+
+	def normalizescores(self, scores, smallIsBetter=0):
+		vsmall=0.00001
+		if smallIsBetter:
+			minscore=min(scores.values())
+			return dict([(u, float(minscore) / max(vsmall, l)) for (u, l) in scores.items()])
+		else:
+			maxscore=max(scores.values())
+			if maxscore == 0: maxscore = vsmall
+			return dict([(u, float(c) / maxscore) for (u, c) in scores.items()])
+
+	def frequencyscore(self, rows):
+		counts=dict([(row[0], 0) for row in rows])
+		for row in rows: counts[row[0]] += 1
+		return self.normalizescores(counts)
+
+	def locationscore(self, rows):
+		locations=dict([(row[0], 1000000) for row in rows])
+		for row in rows:
+			loc=sum(row[1:])
+			if loc < locations[row[0]]: locations[row[0]]=loc
+
+		return self.normalizescores(locations, smallIsBetter = 1)
+
+	def distancescore(self, rows):
+		if len(rows[0]) <= 2: return dict([(row[0], 1.0) for row in rows])
+		
+		mindistance = dict([(row[0], 1000000) for row in rows])		
+		for row in rows:
+			dist=sum([abs(row[i] - row[i - 1]) for i in range(2, len(row))])
+			if dist < mindistance[row[0]]: mindistance[row[0]] = dist
+		
+		return self.normalizescores(mindistance, smallIsBetter = 1)
+
+	def inboundlinkscore(self, rows):
+		uniqueurls=set([row[0] for row in rows])
+		inboundcount=dict([(u, self.con.execute('select count(*) from link where toid=%d' % u).fetchone()[0]) for u in uniqueurls])
+		return self.normalizescores(inboundcount)
 
